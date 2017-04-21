1fa6dda182407f2227ab48d6039f0b268b4297a5
Angie K. Titova <angie.badfox@gmail.com>
2017-04-21 03:25:33 +0300

    [JS] Turn tabs into accordion on mobile

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -26,11 +26,17 @@ $('.js-slider').slick({
 
 
 $('.js-tab').click(function(){  
-	$('.js-tab').attr('aria-selected', 'false');
- 	$(this).attr('aria-selected', 'true');
-    $('.js-tabpanel').attr('aria-hidden', 'true');
-  
-	var tabPanelId = $(this).attr('aria-controls');
-    var $tabPanel = $("#" + tabPanelId);  
-	$tabPanel.attr('aria-hidden', 'false');
- });
+  $('.js-tab').attr('aria-selected', 'false');
+  $(this).attr('aria-selected', 'true');
+  $('.js-tabpanel').attr('aria-hidden', 'true');
+
+  const tabPanelId = $(this).attr('aria-controls');
+  const $tabPanel = $("#" + tabPanelId);  
+  $tabPanel.attr('aria-hidden', 'false');
+});
+
+$('.js-toggle-tab').click(function(){
+  const $tab = $(this).parent();
+  $('.js-tabpanel').attr('aria-hidden', 'true');
+  $tab.attr('aria-hidden', 'false');
+});
